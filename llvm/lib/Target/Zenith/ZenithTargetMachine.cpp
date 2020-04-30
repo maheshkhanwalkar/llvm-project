f938987906bb2f10db1da015c5c5089d7d77ace4
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-04-30 16:24:10 -0400

    [zenith] specify data layout

--- llvm/lib/Target/Zenith/ZenithTargetMachine.cpp
+++ llvm/lib/Target/Zenith/ZenithTargetMachine.cpp
@@ -10,12 +10,21 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeZenithTarget()
   RegisterTargetMachine<ZenithTargetMachine> X(getZenithTarget());
 }
 
+static StringRef makeLayout()
+{
+  /**
+   * E - Big Endian architecture
+   * p:32:32 - 32 bit (pointer, ABI, and preferred alignment)
+   */
+  return "E-p:32:32";
+}
+
 /* FIXME: need to specify a layout description... */
 ZenithTargetMachine::ZenithTargetMachine(const Target &T, const Triple& TT,
     StringRef CPU, StringRef FS, const TargetOptions& Options,
     Optional<Reloc::Model> RM, Optional<CodeModel::Model> CM,
                                          CodeGenOpt::Level OL, bool JIT)
-    : LLVMTargetMachine(T, "", TT, CPU, FS, Options, Reloc::Static,
+    : LLVMTargetMachine(T, makeLayout(), TT, CPU, FS, Options, Reloc::Static,
                         CodeModel::Small, OL),
 
-      Layout("") { }
+      Layout(makeLayout()) { }
